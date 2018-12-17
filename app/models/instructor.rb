ee8b94cab0f9631ad8e618785886c1a15f11e94d
github username <github email address>
2018-12-17 13:04:42 +0000

    first commit

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,40 @@
 class Instructor
 
+@@all = []
+
+attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pass_student(student, test_name)
+    BoatingTest.all.find do |test|
+      if test.student == student && test.test_name == test_name
+        test.status = 'passed'
+        test
+      elsif test.student != student || test.test_name != test.name
+        pass = BoatingTest.new(student, test_name, 'passed', self)
+        pass
+      end
+    end
+  end
+
+  def fail_student(student, test_name)
+    BoatingTest.all.find do |test|
+      if test.student == student && test.test_name == test_name
+        test.status = 'failed'
+        test
+      elsif test.student != student || test.test_name != test_name
+        fail = BoatingTest.new(student, test_name, 'failed', self)
+        fail
+      end
+    end
+  end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,27 @@
 class Student
 
+@@all = []
+
+  attr_accessor :first_name, :student, :test_name, :status
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+ def add_boating_test(student, test_name, status)
+   @student = student
+   @test_name = test_name
+   @status = status
+   @@all << self
+ end
+
+ def self.find_student(first_name)
+   self.all.find {|student| student.first_name}
+ end
+
+ def grade_percentage
+   BoatingTest.all.each
+ end
+
 end
